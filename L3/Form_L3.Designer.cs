f39298511197957e4953e4dbb5a7c22b4808b565
Арина <@ACER-ARINA>
2018-11-05 21:16:13 +0300

    pr

---
+++ L3/Form_L3.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace L3
+{
+    partial class Form_L3
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxR = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxR
+            // 
+            this.textBoxR.Location = new System.Drawing.Point(17, 47);
+            this.textBoxR.Name = "textBoxR";
+            this.textBoxR.Size = new System.Drawing.Size(100, 22);
+            this.textBoxR.TabIndex = 0;
+            this.textBoxR.Text = "50";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "r:";
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(17, 86);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(100, 44);
+            this.buttonRun.TabIndex = 2;
+            this.buttonRun.Text = "Построить";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // Form_L3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1054, 498);
+            this.Controls.Add(this.buttonRun);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxR);
+            this.Name = "Form_L3";
+            this.Text = "Form_L3";
+            this.Load += new System.EventHandler(this.Form_L3_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form_L3_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxR;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonRun;
+    }
+}
+
---
+++ L3/Form_L3.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L3
+{
+    public partial class Form_L3 : Form
+    {
+        public Form_L3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_L3_Paint(object sender, PaintEventArgs e)
+        {
+
+            try
+            {
+                Draw(e.Graphics, new Rectangle(0, 0, Width, Height));
+            }
+            catch
+            {
+                MessageBox.Show("Введены неверные данные!");
+            }
+        }
+
+        public void Draw(Graphics g, Rectangle r)
+        {
+            Bitmap bmp = new Bitmap(r.Width, r.Height);
+            DrawF(bmp, Convert.ToInt32(textBoxR.Text));
+            g.DrawImage(bmp, r);
+            bmp.Dispose();
+        }
+
+        public void DrawF(Bitmap bmp, int r)
+        {
+            DrawFigure.DrawCircle(bmp, bmp.Width / 2, bmp.Height / 2, r, 30, 60);
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void Form_L3_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
