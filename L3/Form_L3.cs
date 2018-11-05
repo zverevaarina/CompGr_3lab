81cf02d58f514abbf1f28cd1e644179234cd1f5a
Дмитрий Донских <steamdiver@yandex.ru>
2018-11-05 19:45:57 +0300

    l3

---
+++ L3/Form_L3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using libDrawing;
+
+namespace L3
+{
+    public partial class Form_L3 : Form
+    {
+        private Drawer drawer;
+        private Segment s = new Segment(new Point(0, 0), 50, 30, 60);
+        public Form_L3()
+        {
+            InitializeComponent();
+            drawer = new Drawer(pictureBox.Width, pictureBox.Height, new Point(pictureBox.Width / 2, pictureBox.Height / 2));
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            var bmp = drawer.Draw(s, true);
+            pictureBox.Image = bmp;
+        }
+
+        private void MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Middle)
+            {
+                drawer.WorldCenter = e.Location;
+                pictureBox.Image = drawer.Draw(s, true);
+            }
+
+            if (e.Button == MouseButtons.Left)
+            {
+                s.Position = new Point(-drawer.WorldCenter.X + e.Location.X, -drawer.WorldCenter.Y + e.Location.Y);
+                pictureBox.Image = drawer.Draw(s, true);
+            }
+        }
+    }
+}
---
+++ libDrawing/Drawer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libDrawing
+{
+    public class Drawer
+    {
+        public Point WorldCenter { get; set; }
+
+        private int _width;
+        private int _height;
+
+        public Drawer(int width, int height, Point worldCenter)
+        {
+            WorldCenter = worldCenter;
+            _width = width;
+            _height = height;
+        }
+
+        public Bitmap Draw(Segment s, bool drawObj = false)
+        {
+            var bmp = new Bitmap(_width, _height);
+            s.Draw(bmp, WorldCenter);
+            if (drawObj)
+                Graphics.FromImage(bmp).DrawEllipse(new Pen(Color.Aqua, 2), WorldCenter.X - 5, WorldCenter.Y - 5, 10, 10);
+            return bmp;
+        }
+    }
+}
